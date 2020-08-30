9ae4c55cb133523ec03d11449b8d26fdcac44521
Sabin Maharjan <sabin.maharjan@caremetx.com>
2020-08-31 02:02:27 +0545

    changes

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -3,50 +3,97 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'alert',
   template: `
-    <h1>JavaScript Progress Bar</h1>
+    <h1>Angular Toast with timer</h1>
 
-    <div id="myProgress">
-      <div id="myBar"></div>
+    <div class="wrapper">
+      <div class="alert" (mouseenter)="stopTimer()" (mouseleave)="resetTimer()">
+        <div class="alert-wrapper">
+          <div class="wrapper2">
+            <div class="alert-bar"></div>
+            <div class="icon"></div>
+          </div>
+          <div class="content">Are you sure you want to exit?</div>
+          <div class="close" (click)="toggleAlert()">&times;</div>
+          <div id="alert-timer"></div>
+        </div>
+      </div>
     </div>
-
+    <button (click)="resetTimer()">Load Timer</button>
+    <button (click)="toggleAlert()">Toggle Timer</button>
     <br>
-    <button (click)="move()">Click Me</button>
   `,
   styles: [
-    `h1 { font-family: Lato; }`,
-    `#myProgress { width: 100%; background-color: #ddd; }`,
-    `#myBar { width: 1%; height: 30px; background-color: #4CAF50; }`
+    `h1 {
+      font-family: Lato;
+    }`,
+
+    `.wrapper { width: 500px; margin: 0 auto; }`,
+    `.alert { transition: transform .3s; } `,
+    `.alert-wrapper {
+        min-height: 50px;
+        margin: 5px;
+        display: flex;
+        padding: 10px;
+        box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.5);
+        position: relative;
+    }`,
+    `.icon { width: 30px; height: 30px; margin-left: 10px; margin-top: 10px; border-radius: 50%; background: green; }`,
+    `.alert-bar { min-height: 50px; min-width: 3px; background: red; display: inline-block; border-radius: 3px; }`,
+    `.content { flex: 1; text-align: left; padding: 15px; }`,
+    `.close { margin-left: auto; cursor: pointer; }`,
+    `.wrapper2 { display: flex; }`,
+    `#alert-timer {
+        width: 100%;
+        height: 3px;
+        position: relative;
+        display: block;
+        background-color: #4CAF50;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+    }`,
+    `.hide { transform: scaleY(0); } `,
   ]
 })
-export class AlertComponent implements OnInit {
 
-  private i = 0;
-  private elem: any;
-  private width: number;
-  private id: any;
-  constructor() { }
+export class AlertComponent implements OnInit {
+  alertElement: any;
+  private timerElement: any;
+  private timerWidth: number;
+  private timerInterval: any;
 
   ngOnInit() {
+    this.alertElement = document.querySelector('div.alert');
+    this.timerElement = document.getElementById('alert-timer');
+    this.resetTimer();
   }
 
-  move() {
-    if (this.i === 0) {
-      this.i = 1;
-      this.elem = document.getElementById('myBar');
-      this.width = 1;
-      this.id = setInterval(this.frame.bind(this), 10);
-    }
+  resetTimer() {
+    this.timerElement = document.getElementById('alert-timer');
+    this.timerWidth = 100;
+    this.timerElement.style.width = this.timerWidth + '%';
+    clearInterval(this.timerInterval);
+    this.timerInterval = setInterval(this.decrementTimer.bind(this), 20);
   }
 
+  stopTimer() {
+    this.timerWidth = 100;
+    this.timerElement.style.width = this.timerWidth + '%';
+    clearInterval(this.timerInterval);
+  }
 
-  frame() {
-      if (this.width >= 100) {
-        clearInterval(this.id);
-        this.i = 0;
+  decrementTimer() {
+      if (this.timerWidth <= 0) {
+        clearInterval(this.timerInterval);
+        this.toggleAlert();
       } else {
-        this.width++;
-        this.elem.style.width = this.width + '%';
+        this.timerWidth--;
+        this.timerElement.style.width = this.timerWidth + '%';
       }
-    }
+  }
+
+  toggleAlert() {
+    this.alertElement.classList.toggle('hide');
+  }
 
 }
